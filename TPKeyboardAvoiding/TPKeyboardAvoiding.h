920f4d56c5610a90bb14bb00a7f975f709440d4e
Dmitry Golubev <dmitry.golubev@vk.team>
2023-10-30 18:43:51 +0300

    Fix public headers

---
+++ TPKeyboardAvoiding/TPKeyboardAvoiding.h
@@ -0,0 +1,24 @@
+//
+//  TPKeyboardAvoiding.h
+//  TPKeyboardAvoiding
+//
+//  Created by Michael Tyson on 30/09/2013.
+//  Copyright © 2023 Pavel Osipov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TPKeyboardAvoiding/TPKeyboardAvoidingCollectionView.h>
+#import <TPKeyboardAvoiding/TPKeyboardAvoidingScrollView.h>
+#import <TPKeyboardAvoiding/TPKeyboardAvoidingTableView.h>
+#import <TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.h>
+#import <TPKeyboardAvoiding/TPKeyboardAvoiding.h>
+
+//! Project version number for TPKeyboardAvoiding.
+FOUNDATION_EXPORT double TPKeyboardAvoidingVersionNumber;
+
+//! Project version string for TPKeyboardAvoiding.
+FOUNDATION_EXPORT const unsigned char TPKeyboardAvoidingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPKeyboardAvoiding/PublicHeader.h>
+
+
